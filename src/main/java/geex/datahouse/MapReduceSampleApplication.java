7d5205c699e59da2b4ba876904c869caa65230d8
guye <guye@geexfinance.cn>
2017-12-09 20:26:42 +0800

    Update: hadoop本地可执行环境

--- src/main/java/geex/datahouse/MapReduceSampleApplication.java
+++ src/main/java/geex/datahouse/MapReduceSampleApplication.java
@@ -1,5 +1,8 @@
 package geex.datahouse;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,10 +13,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 public class MapReduceSampleApplication {
+
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
 
@@ -46,22 +47,17 @@ public class MapReduceSampleApplication {
         }
     }
 
-    public static void main(String args[]) {
-        try {
-            Configuration conf = new Configuration();
-            Job job = Job.getInstance(conf, "word count");
-            job.setJarByClass(MapReduceSampleApplication.class);
-            job.setMapperClass(TokenizerMapper.class);
-            job.setCombinerClass(IntSumReducer.class);
-            job.setReducerClass(IntSumReducer.class);
-            job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(IntWritable.class);
-            FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path(args[1]));
-            System.exit(job.waitForCompletion(true) ? 0 : 1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(MapReduceSampleApplication.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path("D:\\git\\datahouse\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\git\\datahouse\\output"));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
